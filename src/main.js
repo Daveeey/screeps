afdeda6a27e6467df9e74fae2eef8dbba5516c31
Daveeey <33289932+Daveeey@users.noreply.github.com>
2017-11-01 15:40:03 +0000

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,54 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
+module.exports.loop = function () {
+	
+    var creepCount = new Array();
+	creepCount['harvester'] = 0;
+	creepCount['upgrader'] = 0;
+	creepCount['builder'] = 0;
+	
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        } else {
+			var creep = Game.creeps[name];
+			creepCount[creep.memory.role]++;
+		}
+    }
+    
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+    
+    if(Game.spawns['Spawn1'].energy === Game.spawns['Spawn1'].energyCapacity && !Game.spawns['Spawn1'].spawning){
+        // We can build a new unit - we decide based on priority
+        if(creepCount['harvester'] < 4){
+            Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], 'Harvester' + Game.time, {memory: {role: 'harvester'}});
+            console.log('Created new Harvester ');
+            Game.spawns['Spawn1'].say('Building Harvester');
+        }
+        else if(creepCount['upgrader'] < 4){
+            Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], 'Upgrader' + Game.time, {memory: {role: 'upgrader'}});
+            console.log('Created new Builder ');
+            Game.spawns['Spawn1'].say('Building Upgrader');
+        }
+        else if(creepCount['builder'] < 4){
+            Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], 'Builder' + Game.time, {memory: {role: 'builder'}});
+            console.log('Created new Upgrader ');
+            Game.spawns['Spawn1'].say('Building Builder');
+        }
+    }
+    
+}
