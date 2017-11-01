5799888334012ace477b763938070a219302081a
Daveeey <33289932+Daveeey@users.noreply.github.com>
2017-11-01 15:42:24 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
-	console.log('There goes another tick');
     var creepCount = new Array();
 	creepCount['harvester'] = 0;
 	creepCount['upgrader'] = 0;
