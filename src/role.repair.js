9dbcc3eba5888ad9909b1f45188b350b9df0e72c
Dave <david.l@pebbleltd.co.uk>
2017-11-03 15:37:23 +0000

    test

--- src/role.repair.js
+++ src/role.repair.js
@@ -27,7 +27,6 @@ var roleRepair = {
              var source = creep.room.find(FIND_SOURCES);
              if(creep.harvest(source[0]) == ERR_NOT_IN_RANGE) {
                  creep.moveTo(source[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-                 console.log('Moving to source');
              }
          }
         else {
