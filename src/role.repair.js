07cb7ee73366e2bb59fc8ceb60804bd8ba72a85d
Dave <david.l@pebbleltd.co.uk>
2017-11-06 10:47:52 +0000

    test

--- src/role.repair.js
+++ src/role.repair.js
@@ -3,42 +3,33 @@ var roleRepair = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-        if(creep.carry.energy == 0) {
+        if(creep.memory.repairing && creep.carry.energy == 0) {
             creep.memory.repairing = false;
             creep.say('harvest');
         }
 
         if(!creep.memory.repairing && creep.carry.energy == creep.carryCapacity) {
             creep.memory.repairing = true;
-            creep.say('repair');
+            creep.say('upgrade');
         }
-
-        // if(!creep.memory.repairing) {
-        //     var source = creep.pos.findClosestByPath(FIND_SOURCES);
-        //     console.log(source);
-        //     if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-        //         creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
-        //         console.log('Moving to source');
-        //     }
-        // }
         if(!creep.memory.repairing) {
-             var source = creep.room.find(FIND_SOURCES);
-             if(creep.harvest(source[1]) == ERR_NOT_IN_RANGE) {
-                 creep.moveTo(source[1], {visualizePathStyle: {stroke: '#ffaa00'}});
-             }
-         }
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
         else {
-             const targets = creep.room.find(FIND_STRUCTURES, {
-                 filter: object => object.hits < object.hitsMax
-             });
+            const targets = creep.room.find(FIND_STRUCTURES, {
+                filter: object => object.hits < object.hitsMax
+            });
 
-             targets.sort((a,b) => a.hits - b.hits);
+            targets.sort((a,b) => a.hits - b.hits);
 
-             if(targets.length > 0) {
-                 if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
-                     creep.moveTo(targets[0]);
-                 }
-             }
+            if(targets.length > 0) {
+                if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
+            }
         }
     }
 };
