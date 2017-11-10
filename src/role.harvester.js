78ce7b95ae4c0d712879d9870f675f5a59b4276a
Dave <david.l@pebbleltd.co.uk>
2017-11-10 14:05:39 +0000

    test

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -29,8 +29,6 @@ var roleHarvester = {
                 if(targets.length > 0) {
                     if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                    } else {
-                        console.log(creep.transfer(targets[0], RESOURCE_ENERGY));
                     }
                 } else {
                     creep.moveTo(Game.flags.HarvesterHoldingStation);
