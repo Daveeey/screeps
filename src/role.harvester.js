508d236c072e549cbba72aeb6d1dfed78eaf5802
Dave <david.l@pebbleltd.co.uk>
2017-11-10 12:14:18 +0000

    test

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -20,7 +20,19 @@ var roleHarvester = {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             } else {
-                creep.moveTo(Game.flags.HarvesterHoldingStation);
+                targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_CONTAINER) &&
+                            structure.store.energy < structure.storeCapacity;
+                    }
+                });
+                if(targets.length > 0) {
+                    if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                } else {
+                    creep.moveTo(Game.flags.HarvesterHoldingStation);
+                }
             }
         }
 	}
