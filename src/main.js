87b7a179bb45c07c02b3fad0514b12a04d27fe23
Dave <david.l@pebbleltd.co.uk>
2017-12-04 10:57:11 +0000

    test

--- src/main.js
+++ src/main.js
@@ -80,11 +80,11 @@ module.exports.loop = function () {
     }
 
     // Room Defense Basic
-        var hostiles = Game.rooms[roomName].find(FIND_HOSTILE_CREEPS);
+        var hostiles = Game.room.find(FIND_HOSTILE_CREEPS);
         if(hostiles.length > 0) {
             var username = hostiles[0].owner.username;
             Game.notify(`User ${username} spotted in room ${roomName}`);
-            var towers = Game.rooms[roomName].find(
+            var towers = Game.room.find(
                 FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
             towers.forEach(tower => tower.attack(hostiles[0]));
         }
