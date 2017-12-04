1a9ada5b0cf56cf53b53efe75ea41a2599d298d6
Dave <david.l@pebbleltd.co.uk>
2017-12-04 11:24:01 +0000

    test

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -17,10 +17,9 @@ var roleHarvester = {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                console.log('Moving to source');
             }
-            console.log('Moving to source');
         } else {
-            console.log('No source');
             if (targetContainers){
                 var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
