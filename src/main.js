16f15496bff93c0e325984c7eef4bcc766f46fdf
Dave Lambert <david.l@pebbleltd.co.uk>
2018-01-26 15:34:56 +0000

    Changed spawner

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ var roleBuilder = require('role.builder');
 var roleRepair = require('role.repair');
 var roleRunner = require('role.runner');
 
+var spawnModel = require('spawns');
+
 module.exports.loop = function () {
     var creepCount = new Array();
 	creepCount['harvester'] = 0;
@@ -48,35 +50,7 @@ module.exports.loop = function () {
     }
     
     if(!Game.spawns['Spawn1'].spawning){
-        if(creepCount['harvester'] < 4){
-            if(Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,CARRY,MOVE], 'Harvester' + Game.time, {memory: {role: 'harvester'}})){
-                //console.log('Spawning new Harvester ');
-            }
-        }
-        //else if(creepCount['runner'] < 3){
-        //    Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], 'Runner' + Game.time, {memory: {role: 'runner', upgrading: false}});
-        //    console.log('Spawning new Runner ');
-        //
-        //}
-        else if(creepCount['upgrader'] < 2){
-            if(Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], 'Upgrader' + Game.time, {memory: {role: 'upgrader', upgrading: false}})){
-                //console.log('Spawning new Upgrader ');
-            }
-        }
-        else if(creepCount['repairer'] < 2){
-            if(Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], 'Repairer' + Game.time, {memory: {role: 'repairer', repairing: false}})){
-                //console.log('Spawning new Repairer ');
-            }
-        }
-        else if(creepCount['builder'] < 2){
-            const targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                // Only if there is something to build!
-                if(Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], 'Builder' + Game.time, {memory: {role: 'builder', building: false}})){
-                    //console.log('Spawning new Builder ');
-                }
-            }
-        }
+        spawnModel.run();
     }
 
     // Room Defense Basic
