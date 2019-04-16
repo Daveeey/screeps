8292de5110bd8f8079cee2130f1688e71952f1a9
Daveeey <dave@dave-lambert.co.uk>
2019-04-16 09:50:16 +0100

    push to git

--- src/spawns.js
+++ src/spawns.js
@@ -7,16 +7,16 @@ var spawnModel = {
                 console.log('Spawning new Harvester ');
             }
         }
-        else if(creepCount['runner'] < 1 && Game.rooms['W19N45'].energyAvailable >= 300){
-             Game.spawns['Spawn1'].spawnCreep([CARRY,CARRY,CARRY,MOVE,MOVE,MOVE], 'Runner' + Game.time, {memory: {role: 'runner', upgrading: false}});
-             console.log('Spawning new Runner ');
-
-         }
         else if(creepCount['upgrader'] < 2 && Game.rooms['W19N45'].energyAvailable >= 300){
             if(Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE,MOVE,MOVE], 'Upgrader' + Game.time, {memory: {role: 'upgrader', upgrading: false}})){
                 console.log('Spawning new Upgrader ');
             }
         }
+        else if(creepCount['runner'] < 1 && Game.rooms['W19N45'].energyAvailable >= 300){
+             Game.spawns['Spawn1'].spawnCreep([CARRY,CARRY,CARRY,MOVE,MOVE,MOVE], 'Runner' + Game.time, {memory: {role: 'runner', upgrading: false}});
+             console.log('Spawning new Runner ');
+
+         }
         else if(creepCount['builder'] < 1 && Game.rooms['W19N45'].energyAvailable >= 300){
             const targets = Game.rooms['W19N45'].find(FIND_CONSTRUCTION_SITES);
             if(targets.length) {
